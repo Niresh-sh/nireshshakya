c622e259eace3504a752e48bd7d225a387e89305
Niresh-sh <neerace.sh@gmail.com>
2025-12-24 17:13:20 +0545

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Portfolio from "./Pages/Portfolio"
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
  
   return (
     <>
    <Portfolio />
+   <Analytics />
     </>
   )
 }
